1369d8cf6ec5078723e4a8c69744e1a044153d34
mrKORKMAZ <musa.krkmz6638@gmail.com>
2023-06-13 20:29:13 +0300

    tax entry

---
+++ app.js
@@ -0,0 +1,10 @@
+// const taxRate = 0.18;
+// const shippingPrice = 25.99;
+// const freeShippingPrice = 3000;
+
+//localStorage vs. sessionStorage
+// window.addEventListener("load". ()=>{
+//     localStorage.setItem("taxRate", taxRate);
+//     localStorage.setItem("shippingPrice", shippingPrice);
+//     localStorage.setItem("freeShippingPrice", freeShippingPrice);
+// })
